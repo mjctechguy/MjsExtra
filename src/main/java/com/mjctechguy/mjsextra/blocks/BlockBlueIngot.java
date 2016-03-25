3d49b76441ab5b88bd131db5f0e499230527e362
mjctechguy <mjctechguy@gmail.com>
2016-03-25 15:48:14 +0000

    Add recpies and blocks

--- src/main/java/com/mjctechguy/mjsextra/blocks/BlockBlueIngot.java
+++ src/main/java/com/mjctechguy/mjsextra/blocks/BlockBlueIngot.java
@@ -3,7 +3,6 @@ package com.mjctechguy.mjsextra.blocks;
 import net.minecraft.block.material.Material;
 
 public class BlockBlueIngot extends BlockMjsExtra {
-    //Yes
     public BlockBlueIngot(){
         super(Material.rock,"blueIngotBlock");
     }
